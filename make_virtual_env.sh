bf8d70167f57b778fc8a277ac16348627e8bc586
shurik <shurik@skotch.nirvana>
2023-03-24 14:07:18 +0300

    kaka

---
+++ make_virtual_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! [[ -z "${VIRTUAL_ENV}" ]]; then
+  echo "already in virtual env"
+  exit
+fi
+
+ENV_FILE=venv/bin/activate
+if test -f "$ENV_FILE"; then
+	echo "$ENV_FILE exists."
+else
+	python3 -m venv venv
+fi
+
+
+source "$ENV_FILE"
+pip3 install -r requirements.txt
+
+
