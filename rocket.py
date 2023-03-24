0648a9e331d779a7d700d95af0cfc8331c6d9881
shurik <shurik@skotch.nirvana>
2023-03-24 15:32:17 +0300

    start

--- rocket.py
+++ rocket.py
@@ -1,7 +1,8 @@
 
 import numpy as np
+import pandas as pd
 from dataclasses import dataclass, field
-from typing import NewType, List
+from typing import NewType, List, Callable
 
 F_g = 9.8
 
@@ -25,10 +26,12 @@ class RocketParams:
 RocketParamsType = NewType('RocketStageType', RocketStage)
 
 class Rocket:
-    def __init__(self, params: RocketParamsType = None):
+    def __init__(self, params: RocketParamsType = None, angle_func: Callable[float, float] = lambda _: 0.):
         self._max_times = np.zeros(0)
+        self._calcs = pd.DataFrame({"v":np.zeros(0),"m":np.zeros(0),"h":np.zeros(0),"x":np.zeros(0),"angle":np.zeros(0)})
         if params is not None:
             self.set_params(params)
+        self.set_angle_func(angle_func)
 
     def set_params(self, params: RocketParamsType) -> None:
         self._params = params
@@ -39,6 +42,8 @@ class Rocket:
         for idx, stage in enumerate(_params.stages):
             self._max_times[idx] = stage.max_time()
         
+    def set_angle_func(self, func : Callable[float, float]) -> None:
+        self._angle_func = func
 
     def curr_stage(self, t:float) -> RocketStageType:
         tmp_t = t
@@ -49,4 +54,7 @@ class Rocket:
         else:
             return None #?
         return self._params.stages[idx]
+
+    def calc(self, step = 1) -> None:
+
 
